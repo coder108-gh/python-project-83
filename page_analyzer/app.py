6e463aa69b6fb4fa39aa76289313add60175509d
coder108 <eugene_kumin@mail.ru>
2025-03-22 12:26:13 +0300

    secret key

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
 
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!-py-83</p>"
+    return '<p>Hello, World!-py-83</p>'
