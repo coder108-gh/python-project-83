c00e035df364a1c2fc2d6c7c0f5a71bd2567ea1b
coder108 <eugene_kumin@mail.ru>
2025-03-26 09:47:46 +0300

    st3.1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,6 @@
 import os
 
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 
@@ -7,9 +8,15 @@ app = Flask(__name__)
 
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def hello_world():
     return '<p>Hello, World!-py-83</p>'
+
+
+
+
+
 
