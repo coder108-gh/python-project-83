54c969569fa74a59fcb27d5668c1e776d5b93a5e
coder108 <eugene_kumin@mail.ru>
2025-04-10 18:20:54 +0300

    iterm step6

--- page_analyzer/pars.py
+++ page_analyzer/pars.py
@@ -1,8 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
+
+
+def get_if_exists(soup: BeautifulSoup, tag_name: str, def_value=''):
+    result = soup.select_one(tag_name)
+    if result is None:
+        return def_value
+    return result.text[:255]
+
+
+def process_html(html_doc: str):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    result = {}
+    result['h1'] = get_if_exists(soup, 'h1')
+    result['title'] = get_if_exists(soup, 'title')
+    result['descr'] = ''
+
+    meta_name = soup.find('meta', attrs={'name': "description"})
+    if meta_name is not None:
+        cont = meta_name.get('content')
+        if cont is not None:
+            result['descr'] = cont[:255]
+    return result
 
 
 def get_check_data(url):
-    code = None
     try:
         r = requests.get(url)
         r.raise_for_status()
@@ -12,4 +34,5 @@ def get_check_data(url):
     except Exception as e:
         raise e
 
-    return {"h1": "", "title": "", "code": code, "descr": ""}
+    return {'code': code, 'h1': '', 'title': '', 'descr': ''} | process_html(r.text)
+
